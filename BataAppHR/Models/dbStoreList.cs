b1901ae58bf69b4620000ff317dd2d2d6afba373
atresnaprana <48087980+atresnaprana@users.noreply.github.com>
2023-04-20 16:08:54 +0700

    added store list table

---
+++ BataAppHR/Models/dbStoreList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+
+namespace OnPOS.Models
+{
+    public class dbStoreList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int COMPANY_ID { get; set; }
+        public string STORE_NAME { get; set; }
+        public string STORE_ADRESS { get; set; }
+        public string STORE_CITY { get; set; }
+        public string STORE_PROVINCE { get; set; }
+        public string STORE_POSTAL { get; set; }
+        public string STORE_BANK_NAME { get; set; }
+        public string STORE_BANK_NUMBER { get; set; }
+        public string STORE_BANK_BRANCH { get; set; }
+        public string STORE_BANK_COUNTRY { get; set; }
+        public DateTime STORE_REG_DATE { get; set; }
+        public string STORE_BL_FLAG { get; set; }
+        public DateTime ENTRY_DATE { get; set; }
+        public DateTime UPDATE_DATE { get; set; }
+        public string ENTRY_USER { get; set; }
+        public string UPDATE_USER { get; set; }
+        public string FLAG_AKTIF { get; set; }
+        public byte[] FILE_PHOTO { get; set; }
+
+        public string FILE_PHOTO_NAME { get; set; }
+        
+        [NotMapped]
+        public bool isBlackList { get; set; }
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile filePhoto { get; set; }
+        [NotMapped]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [NotMapped]
+        public string errmsg { get; set; }
+    }
+}
