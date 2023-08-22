258eb46fadf7dfa98b24604d805577b4f9537573
atresnaprana <48087980+atresnaprana@users.noreply.github.com>
2023-08-22 22:31:41 +0700

    added index page and initiated item master controller

--- BataAppHR/Controllers/ItemMasterController.cs
+++ BataAppHR/Controllers/ItemMasterController.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using BataAppHR.Data;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using MimeDetective.Definitions;
+using MimeDetective.Diagnostics;
+using MimeDetective.Engine;
+using MimeDetective.Storage;
+using System.Net;
+using OnPOS.Models;
+using static System.Net.WebRequestMethods;
+using OfficeOpenXml;
 namespace OnPOS.Controllers
 {
     public class ItemMasterController : Controller
     {
+        private readonly FormDBContext db;
+        private IHostingEnvironment Environment;
+        private readonly ILogger<StoreListController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public IConfiguration Configuration { get; }
+
+
+        public ItemMasterController(FormDBContext db, ILogger<StoreListController> logger, IHostingEnvironment _environment, UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            logger = logger;
+            Environment = _environment;
+            this.db = db;
+            Configuration = configuration;
+
+        }
+        [Authorize(Roles = "CustomerOnPos")]
         public IActionResult Index()
         {
-             return View();
+            var data = db.CustomerTbl.Where(y => y.Email == User.Identity.Name).FirstOrDefault();
+            var getItemList = db.ItemMasterTbl.Where(y => y.COMPANY_ID == data.COMPANY_ID).ToList();
+            return View(getItemList);
         }
     }
 }
