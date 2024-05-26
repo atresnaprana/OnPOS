bb19391b89fac9a668499fdd3c4dc8068c12495b
atresnaprana <48087980+atresnaprana@users.noreply.github.com>
2024-05-26 10:46:05 +0700

    commit customer page

---
+++ BataAppHR/Controllers/StockEnablerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BataAppHR.Data;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using MimeDetective.Definitions;
+using MimeDetective.Diagnostics;
+using MimeDetective.Engine;
+using MimeDetective.Storage;
+using System.Net;
+using OnPOS.Models;
+using static System.Net.WebRequestMethods;
+using OfficeOpenXml;
+using BataAppHR.Models;
+using MimeDetective.Storage.Xml.v2;
+
+namespace OnPOS.Controllers
+{
+    public class StockEnablerController : Controller
+    {
+        private readonly FormDBContext db;
+        private IHostingEnvironment Environment;
+        private readonly ILogger<StockEnablerController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public IConfiguration Configuration { get; }
+
+
+        public StockEnablerController(FormDBContext db, ILogger<StockEnablerController> logger, IHostingEnvironment _environment, UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            logger = logger;
+            Environment = _environment;
+            this.db = db;
+            Configuration = configuration;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
